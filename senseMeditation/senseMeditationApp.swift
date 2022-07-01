01b304802b4774f8c0cdf61edef2012dc52ed762
beratios <beratgultekin34@gmail.com>
2022-07-01 18:57:17 +0300

    Initial Commit

---
+++ senseMeditation/senseMeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  senseMeditationApp.swift
+//  senseMeditation
+//
+//  Created by Berat Gültekin on 1.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct senseMeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
